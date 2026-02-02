3009a8362762f46be9f0ee34d567edb82864725a
hyma999 <hymaaritakula93@gmail.com>
2026-02-02 17:34:50 +0530

    added catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -61,7 +61,7 @@ VALIDATE $? "Uzip catalogue code"
 npm install  &>>$LOGS_FILE
 VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
+cp $SCRIPT_DIR/catalogue.services /etc/systemd/system/catalogue.services
 VALIDATE $? "Created systemctl service"
 
 systemctl daemon-reload
